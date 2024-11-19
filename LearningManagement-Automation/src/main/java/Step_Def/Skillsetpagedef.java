bcfefe3a63b9e6cc9727534e8aa21f4281d05a81
NoonapallyAkila <144780088+NoonapallyAkila@users.noreply.github.com>
2024-11-19 18:35:20 +0530

    Update Skillsetpagedef.java

--- LearningManagement-Automation/src/main/java/Step_Def/Skillsetpagedef.java
+++ LearningManagement-Automation/src/main/java/Step_Def/Skillsetpagedef.java
@@ -2,6 +2,31 @@ package Step_Def;
 
 public class Skillsetpagedef {
 	
-	
+	 @Given("the user clicks skillset menu item")
+   public void skillsetmenu(){
+
+       System.out.println("navigated to skillset management");
+    }
+
+    @When("successfully added skillset")
+    public void addskillsetpopup(String skillsetname ) {
+       
+        System.out.println("skillset added successfully");
+    }
+
+    @When("successfully edited skillset")
+    public void editpracticepopup(String email1) 
+        System.out.println("skillset edited successfully");
+    }
+
+    @when("successfully deleted skillset")
+    public void deleteskillsetpopup() {
+      System.out.println("skillset deleted successfully");
+       
+       
+    }
+    @Then("user should be on skillset page")
+       public void skillsetmenu(){
+    System.out.println("user should be skillset management");
 
 }
